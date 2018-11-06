c8348e1500433bd0aa0d8764292d1878b9ac7b47
Joseph Lee <jlee01@stuy.edu>
2018-11-06 16:23:25 -0500

    added stuff

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -12,18 +12,34 @@ public class ArrayMethods{
     //returns the sum of the elements in Row x of ary.
 
    public static int columnSum(int[][] ary, int x){
-     
+     int sum = 0;
+     for (int i = 0; i < ary.length; i++){
+       if (ary[i].length >= x){
+         sum += ary[i][x];
+       }
+     }
+     return sum;
    }
     //returns the sum of the elements in Column x of ary (careful with rows of different lengths!).
 
    /*
    *PART 2 - use prior methods where appropriate
    */
-   public static int[] allRowSums(int[][] ary){}
+   public static int[] allRowSums(int[][] ary){
+     int sum = 0;
+     for (int i = 0; i < ary.length; i++){
+        sum += rowSum(ary , i);
+      }
+    }
     //returns an array of the row sums for each row of ary.
     //Index i of the return array contains the sum of elements in row i.
 
-    public static int[] allColSums(int[][] ary){}
+    public static int[] allColSums(int[][] ary){
+      int sum = 0;
+      for (int i = 0; i < ary.length; i++){
+         sum += columnSum(ary , i);
+       }
+     }
     //Returns an array with the column sum of each column of ary.
     //When a row is not long enough to reach the column count it as a zero. (NO indexOutOfBounds should ever occur)
     //Index i of the return array contains the sum of elements in column i, ignoring any rows that are too short.
